84d0f3ae4fcc3cdc6de4db1f21167a850506b125
Farbod Taherifard <farbod.taherifard@gmail.com>
2025-06-13 10:40:22 +0330

    PersianValidationServiceProvider.php

--- src/PersianValidationServiceProvider.php
+++ src/PersianValidationServiceProvider.php
@@ -8,17 +8,17 @@ class PersianValidationServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // انتشار فایل‌های زبان به پوشه lang در ریشه پروژه
+       
         $this->publishes([
             __DIR__ . '/../lang' => base_path('lang'),
         ], 'persian-validation-lang');
 
-        // بارگذاری فایل‌های زبان
+     
         $this->loadTranslationsFrom(__DIR__ . '/../lang', 'persian-validation');
     }
 
     public function register()
     {
-        // در صورت نیاز، سرویس‌ها را اینجا ثبت کنید
+       
     }
 }
